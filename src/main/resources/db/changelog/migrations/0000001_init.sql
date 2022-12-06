3ac9ee0f8be42eed3e3692651d88e4e4db378b77
Sylvain Mahé <sylv.mahe@gmail.com>
2022-12-06 17:11:49 +0100

    added liquibase + prod conf

---
+++ src/main/resources/db/changelog/migrations/0000001_init.sql
@@ -0,0 +1,61 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.0 (Debian 15.0-1.pgdg110+1)
+-- Dumped by pg_dump version 15.0 (Debian 15.0-1.pgdg110+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: boulder; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.boulder (
+    id uuid NOT NULL,
+    name character varying(255),
+    estimated_grade character varying(255),
+    holds text
+);
+
+
+ALTER TABLE public.boulder OWNER TO postgres;
+
+--
+-- Name: boulder boulder_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.boulder
+    ADD CONSTRAINT boulder_pkey PRIMARY KEY (id);
+
+--
+-- PostgreSQL database dump complete
+--
+
